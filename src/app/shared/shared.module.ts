4031d78206620db3500e99127cadce205a63ee6a
estebancolorado <esteban.colorado@ceiba.com.co>
2022-07-01 17:02:15 -0500

    Refactoring 1.5

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { MensajeErrorCamposDirective } from './directivas/error-campos/directiva/mensaje-error-campos.directive';
 import { MensajeErrorCamposSubmitDirective } from './directivas/error-campos/directiva/mensaje-error-campos-submit.directive';
 import { MensajeErrorCamposContenedorDirective } from './directivas/error-campos/directiva/mensaje-error-campos-contenedor.directive';
-import { ErrorCamposPlantillaComponent } from './directivas/error-campos/componente/error-campos-plantilla.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TrackByPipe } from './pipe/track-by.pipe';
 
 @NgModule({
   declarations: [
-    ErrorCamposPlantillaComponent,
     MensajeErrorCamposDirective,
     MensajeErrorCamposContenedorDirective,
     MensajeErrorCamposSubmitDirective,
